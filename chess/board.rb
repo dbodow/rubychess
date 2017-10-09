66e15bd268163be6563c5f3b3b0621a56430a073
David Bodow <dbodow@gmail.com>
2017-10-09 15:34:23 -0700

    Add Completed Phase 1

---
+++ chess/board.rb
@@ -0,0 +1,35 @@
+require 'byebug'
+require_relative 'piece'
+
+class InvalidMove < StandardError
+
+end
+
+class Board
+  def initialize
+    @grid = Array.new(4) {Array.new(8){nil} }
+    2.times {@grid.unshift(Array.new(8){Piece.new})}  # white pieces
+    2.times {@grid.push(Array.new(8){Piece.new})}     # black pieces
+  end
+
+  def move_piece(start_pos, end_pos)
+    # debugger
+    candidate_piece = self[start_pos]
+    candidate_dest = self[end_pos]
+
+    raise InvalidMove.new "No starting piece" if candidate_piece.nil?       # Create 'nil' piece
+    raise InvalidMove.new "You can't move on top of a piece" if candidate_dest.is_a?(Piece)    # Update after we create 'nil' piece
+
+    self[start_pos], self[end_pos] = self[end_pos], self[start_pos]
+  end
+
+  def [](pos)
+    x, y = pos
+    @grid[x][y]
+  end
+
+  def []=(pos, val)
+    x, y = pos
+    @grid[x][y] = val
+  end
+end
